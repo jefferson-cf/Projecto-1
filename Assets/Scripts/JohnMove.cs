08f0aba7aaf17238ddd47c6a849e77d26e86f1ee
jefferson <cruz.flores.work@outlook.com>
2025-04-20 00:18:33 -0500

    Función para saltar y desplazarse

--- Assets/Scripts/JohnMove.cs
+++ Assets/Scripts/JohnMove.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 
 public class JohnMove : MonoBehaviour
-
 {
+    public float Speed;
+    public float JumpForce;
     private Rigidbody2D Rigidbody2D;
     private float Horizontal;
+    private bool Grounded;
+
     private void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
@@ -12,6 +15,21 @@ public class JohnMove : MonoBehaviour
     private void Update()
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.2f))
+        {
+            Grounded = true;
+        }
+        else Grounded = false;
+
+        if (Input.GetKeyDown(KeyCode.Space) && Grounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        Rigidbody2D.AddForce(Vector2.up * JumpForce);
     }
 
     private void FixedUpdate()
